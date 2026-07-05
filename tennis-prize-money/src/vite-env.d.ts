6b79b4fd87028b61ee8f7be038e9f2a986332e91
Vansh Kumar <vanshkumar95@gmail.com>
2026-07-05 13:36:02 -0700

    chore: remove tennis prize money app from parent repo

--- tennis-prize-money/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_REFRESH_DISPATCH_URL?: string;
-  readonly VITE_REFRESH_DOCS_URL?: string;
-}
--- tennis-prize-money/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/tennis-prize-money/',
-  plugins: [react()],
-});
